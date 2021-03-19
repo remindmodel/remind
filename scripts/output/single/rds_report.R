86b10d2d918874acdfcb6a75dec95b5081d73fd9
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-03-19 12:45:09 +0100

    preventing future bugs and speed increase in rds_report.R

--- scripts/output/single/rds_report.R
+++ scripts/output/single/rds_report.R
@@ -14,7 +14,7 @@ options("magclass.verbosity" = 1)
 
 ############################# BASIC CONFIGURATION #############################
 if(!exists("source_include")) {
-  outputdir <- "output/r8473-trunk-C_Budg600-rem-5/"
+  outputdir <- "output/default_2021-03-18_18.16.40/"
   readArgs("outputdir")
 }
 
@@ -30,12 +30,13 @@ resultsarchive <- "/p/projects/rd3mod/models/results/remind"
 mif <- path(outputdir,paste0("REMIND_generic_",cfg$title,".mif"))
 
 if(file.exists(mif)) {
-  report <- read.report(mif,as.list = FALSE)
+  report <- read.quitte(mif)
 } else {
   report <- convGDX2MIF(gdx,gdx_ref,scenario=cfg$title)
 }
 
-q <- as.quitte(report)
+if (!is.quitte(report)) report <- as.quitte(report)
+q<-report
 if(all(is.na(q$value))) stop("No values in reporting!")
 saveRDS(q,file=rds)
 
