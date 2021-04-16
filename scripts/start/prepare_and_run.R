239c1b8c018b4409ccbb142813761c40c2bb8caf
Michaja Pehl <michaja.pehl@pik-potsdam.de>
2021-04-16 08:48:38 +0200

    remove disgusting trailing underscore I introduced accidentally

--- scripts/start/prepare_and_run.R
+++ scripts/start/prepare_and_run.R
@@ -392,7 +392,7 @@ prepare <- function() {
       cat("Your input data are outdated or in a different regional resolution. New data are downloaded and distributed. \n")
       # add CES data to the list of inputs
       ceshash <- cfg$CESversion
-      cestar <- paste0("CESparameters_",ceshash,"_.tgz")
+      cestar <- paste0("CESparameters_",ceshash,".tgz")
       input_data <- c(input_new, cestar)
       download_distribute(files        = input_data,
                           repositories = cfg$repositories, # defined in your local .Rprofile or on the cluster /p/projects/rd3mod/R/.Rprofile
