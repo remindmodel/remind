64f8c2f922e5321dc549a22f44a9dc326f3ab085
David Klein <dklein@pik-potsdam.de>
2022-02-09 09:58:34 +0100

    Rename parameter to comply with changes in 3c4254840b57e458db1c7c01cd7f60be67ef1e3e

--- scripts/output/comparison/plot_compare_iterations.R
+++ scripts/output/comparison/plot_compare_iterations.R
@@ -142,7 +142,7 @@ plot_iterations <- function(runname) {
 						 legend_position="right")
 
 	### LUC EMISSIONS (MAgPIE) ###
-	emi <- read_all(gdx_path,readpar,name="p_macBaseMagpie",as.list=FALSE)[,,"co2luc"]*1000*44/12
+	emi <- read_all(gdx_path,readpar,name="pm_macBaseMagpie",as.list=FALSE)[,,"co2luc"]*1000*44/12
   emi <- mbind(emi,dimSums(emi,dim=1))
   getNames(emi) <- gsub(".*rem-","",getNames(emi))
 
