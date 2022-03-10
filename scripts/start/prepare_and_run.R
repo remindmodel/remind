e0cd0db258290cac40165e80ad2c1d77985d278c
Renato-Rodrigues <renato.rodrigues@pik-potsdam.de>
2022-03-10 17:41:43 +0100

    filtering out CCU items from loading values to avoid error if CCU is off in a run that uses results from another CCU enabled run

--- scripts/start/prepare_and_run.R
+++ scripts/start/prepare_and_run.R
@@ -672,6 +672,21 @@ prepare <- function() {
                                 list(c("q47_implFETax.M", "!!q47_implFETax.M")))
     }
 
+    # filter out CCU items
+    if(cfg$gms$CCU == 'off'){
+      levs_manipulateThis <- c(levs_manipulateThis,
+                               list(c("v39_shSynTrans.L","!!v39_shSynTrans.L")),
+                               list(c("v39_shSynGas.L","!!v39_shSynGas.L"))
+                              )
+                               
+      margs_manipulateThis <- c(margs_manipulateThis,
+                                list(c("q39_emiCCU.M", "!!q39_emiCCU.M")),
+                                list(c("q39_shSynTrans.M", "!!q39_shSynTrans.M")),
+                                list(c("q39_shSynGas.M", "!!q39_shSynGas.M")),
+                                list(c("q39_EqualSecShare_BioSyn.M", "!!q39_EqualSecShare_BioSyn.M"))
+                                )
+    }
+
     # Include fixings (levels) and marginals in full.gms at predefined position 
     # in core/loop.gms.
     full_manipulateThis <- c(full_manipulateThis,
