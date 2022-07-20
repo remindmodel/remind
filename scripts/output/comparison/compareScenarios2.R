7045979253e96a93698ab6b7445641acfde1f237
Christof Schoetz <cschoetz@pik-potsdam.de>
2022-07-20 16:16:04 +0200

    format displayed text

--- scripts/output/comparison/compareScenarios2.R
+++ scripts/output/comparison/compareScenarios2.R
@@ -17,7 +17,7 @@ chooseFromSequence <- function(sequence, title, default) {
     stop("Choose numbers between 1 and ", length(sequence))
   }
   chosenElements <- if (length(ids) == 0) default else sequence[ids]
-  cat("\nchosen elements:", paste(chosenElements, collapse=", "))
+  cat("\nchosen elements:", paste(chosenElements, collapse=", "), "\n\n")
   return(chosenElements)
 }
 
