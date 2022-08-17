e923adf7a6faf6bbe364d5aedae670b805d77470
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-08-17 17:32:31 +0200

    suppress here::i_am message

--- scripts/utils/restoreRenv.R
+++ scripts/utils/restoreRenv.R
@@ -6,7 +6,7 @@ local({
   if (length(argv) == 1) {
     lockfile <- argv
   } else {
-    here::i_am(file.path("scripts", "utils", "restoreRenv.R"))
+    suppressMessages(here::i_am(file.path("scripts", "utils", "restoreRenv.R")))
     lockfiles <- c(list.files(here::here("archive"), full.names = TRUE),
                    Filter(file.exists, file.path(list.files(here::here("output"), full.names = TRUE), "renv.lock")))
     message(paste0(seq_along(lockfiles), ": ",
@@ -18,7 +18,7 @@ local({
   }
 
   if (!isTRUE(file.exists(lockfile))) {
-    stop("lockfile '", lockfile, "' does not exist.")
+    stop("lockfile '", file.path(normalizePath("."), lockfile), "' does not exist.")
   }
   renv::restore(lockfile = lockfile, clean = TRUE, prompt = FALSE)
   renv::snapshot(prompt = FALSE) # ensure main renv.lock is in sync with library
