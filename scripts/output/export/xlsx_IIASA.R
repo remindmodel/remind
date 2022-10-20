01732e73f45433c5731c4f7afa6a52f184fac7f5
orichters <oliverr@pik-potsdam.de>
2022-10-20 11:14:30 +0200

    add summation checks, use sink()

--- scripts/output/export/xlsx_IIASA.R
+++ scripts/output/export/xlsx_IIASA.R
@@ -18,22 +18,17 @@ library(stringr) # str_sub, str_split
 library(tibble)
 library(tidyr)
 
-meslog <- function(text, LOGFILE) {
-  message(text)
-  write(text, file = LOGFILE, append = TRUE)
-}
-
 options(warn = 1)
 
-MODEL <- "REMIND 3.0"                            # modelname in final file
-REMOVE_FROM_SCEN <- ""                           # you can use regex such as: "_diff|_expoLinear"
-ADD_TO_SCEN <- NULL                              # is added at the beginning
+model <- "REMIND 3.0"                            # modelname in final file
+removeFromScen <- ""                           # you can use regex such as: "_diff|_expoLinear"
+addToScen <- NULL                              # is added at the beginning
 
 # filenames relative to REMIND main directory (or use absolute path) 
-MAPPING <- NULL                                  # file obtained from piamInterfaces, or AR6/SHAPE/NAVIGATE or NULL to get asked
+mapping <- NULL                                  # file obtained from piamInterfaces, or AR6/SHAPE/NAVIGATE or NULL to get asked
 iiasatemplate <- "yaml_or_xlsx_file_from_IIASA"  # provided for each project, can be yaml or xlsx with a column 'Variable'
 
-# note: you can also pass all these options to output.R, so 'Rscript output.R LOGFILE=mylogfile.txt' works.
+# note: you can also pass all these options to output.R, so 'Rscript output.R logFile=mylogfile.txt' works.
 lucode2::readArgs("project")
 
 ### Load project-specific settings
@@ -42,183 +37,193 @@ if (! exists("project")) {
 } else {
   message("# Overwrite settings with project settings for '", project, "'.")
   if ("NGFS_v3" %in% project) {
-    MODEL <- "REMIND-MAgPIE 3.0-4.4"
-    MAPPING <- c("AR6", "AR6_NGFS")
+    model <- "REMIND-MAgPIE 3.0-4.4"
+    mapping <- c("AR6", "AR6_NGFS")
     iiasatemplate <- "../ngfs-internal-workflow/definitions/variable/variables.yaml"
-    REMOVE_FROM_SCEN <- "C_|_bIT|_bit|_bIt"
+    removeFromScen <- "C_|_bIT|_bit|_bIt"
     filename_prefix <- "NGFS"
   } else if ("ENGAGE_4p5" %in% project) {
-    MODEL <- "REMIND 3.0"
-    MAPPING <- c("AR6", "AR6_NGFS")
+    model <- "REMIND 3.0"
+    mapping <- c("AR6", "AR6_NGFS")
     iiasatemplate <- "ENGAGE_CD-LINKS_template_2019-08-22.xlsx"
-    REMOVE_FROM_SCEN <- "_diff|_expoLinear"
+    removeFromScen <- "_diff|_expoLinear"
   } else {
     message("# Command line argument project='", project, "' defined, but not understood.")
   }
 }
 
 # overwrite settings with those specified as command-line arguments
-lucode2::readArgs("outputdirs", "filename_prefix", "OUTPUT_FILENAME", "MODEL",
-                  "MAPPING", "LOGFILE", "REMOVE_FROM_SCEN", "ADD_TO_SCEN", "iiasatemplate")
+lucode2::readArgs("outputdirs", "filename_prefix", "outputFilename", "model",
+                  "mapping", "logFile", "removeFromScen", "addToScen", "iiasatemplate")
 
 # variables to be deleted although part of the template
 temporarydelete <- NULL # example: c("GDP|MER", "GDP|PPP")
 
 ### define filenames
 
+outputFolder <- file.path("output", "export")
+if (! dir.exists(outputFolder)) dir.create(outputFolder, recursive = TRUE)
 tstamp = format(Sys.time(), "%Y-%m-%d_%H.%M.%S")
 if (! exists("filename_prefix")) filename_prefix <- ""
 if (! exists("outputdirs")) outputdirs <- dirname(Sys.glob(file.path("output", "*", "fulldata.gdx")))
-if (! exists("OUTPUT_FILENAME")) {
-  OUTPUT_FILENAME <- file.path("output", paste0(c(strsplit(MODEL, " ")[[1]][[1]], filename_prefix, tstamp), collapse = "_"))
+if (! exists("outputFilename")) {
+  outputFilename <- file.path(outputFolder, paste0(c(strsplit(model, " ")[[1]][[1]], filename_prefix, tstamp), collapse = "_"))
 } else {
-  OUTPUT_FILENAME <- gsub("\\.mif$|\\.xlsx$", "", OUTPUT_FILENAME)
+  outputFilename <- gsub("\\.mif$|\\.xlsx$", "", outputFilename)
 }
-OUTPUT_mif  <- paste0(OUTPUT_FILENAME, ".mif")
-OUTPUT_xlsx <- paste0(OUTPUT_FILENAME, ".xlsx")
-if (! exists("LOGFILE")) LOGFILE <- paste0(OUTPUT_FILENAME, ".log")
+OUTPUT_mif  <- paste0(outputFilename, ".mif")
+OUTPUT_xlsx <- paste0(outputFilename, ".xlsx")
+if (! exists("logFile")) logFile <- paste0(outputFilename, ".log")
 
-### generate mapping if not specified
+message("### Find various logs in ", logFile)
+withCallingHandlers({ # piping messages to logFile
 
-if (is.null(MAPPING) || ! all(file.exists(MAPPING))) {
-  if (all(MAPPING %in% names(piamInterfaces::templateNames()))) {
-    MAPPINGfile <- file.path("output", paste0(paste0(c("mapping", if (is.null(project)) MAPPING else project), collapse = "_"), ".csv"))
-  } else {
-    stop("# MAPPING = '", paste(MAPPING, collapse = ","), "' not understood. ",
-         "Specify a file or templates such as ", paste0(names(piamInterfaces::templateNames()), collapse = ","))
-  }
-  meslog(paste0("### Generate ", MAPPINGfile, " based on piamInterfaces templates ", paste(MAPPING, collapse = ","), "."), LOGFILE)
-  mapreturn <- piamInterfaces::generateMappingfile(templates = MAPPING, outputDirectory = ".",
-                               fileName = MAPPINGfile, model = MODEL, commentFileName = NULL)
-  MAPPING <- MAPPINGfile
-  write(paste0("\n\n\n### Comments from piamInterfaces::generateMappingfile\n\n",
-        paste(paste0("- ", mapreturn[["comments"]]$Variable, ": ", mapreturn[["comments"]]$Comment), collapse = "\n"), "\n\n"),
-        file = LOGFILE, append = TRUE)
-  message("# Find log of piamInterfaces::generateMappingfile() in ", LOGFILE)
-}
-
-meslog(paste0("\n### Generating ", OUTPUT_mif, " and .xlsx using mapping ", MAPPING, "."), LOGFILE)
-message("# Find log of iamc::write.reportProject() in ", LOGFILE)
-
-### define filenames
-
-gdxs <- file.path(outputdirs, "fulldata.gdx")
-scenNames <- getScenNames(outputdirs)
-mif_path <- file.path(outputdirs, paste0("REMIND_generic_", scenNames, ".mif"))
-mif_path_polCosts <- file.path(outputdirs, paste0("REMIND_generic_", scenNames, "_adjustedPolicyCosts.mif"))
-mif_path <- ifelse(file.exists(mif_path_polCosts), mif_path_polCosts, mif_path)
-
-existingfiles <- file.exists(mif_path)
-if (! all(existingfiles)) {
-  meslog(paste0("\n### These mif files cannot be found and will be skipped:\n- ",
-          paste0(mif_path[! existingfiles], collapse = "\n- ")), LOGFILE)
-  mif_path <- mif_path[existingfiles]
-  gdxs <- gdxs[existingfiles]
-  scenNames <- scenNames[existingfiles]
-}
-meslog(paste0("\n### These mif files are used:\n- ", paste0(mif_path, collapse = "\n- ")), LOGFILE)
-
-filename_remind2_mif <- paste0(OUTPUT_FILENAME, "_remind2.mif")
-
-meslog("\n### Read mif files...", LOGFILE)
-
-all_mifs <- NULL
-for (mif in mif_path) {
-  tmp1 <- read.report(mif, as.list=FALSE)
-  # remove -rem-xx and mag-xx from scenario names
-  getNames(tmp1, dim = 1) <- gsub("-(rem|mag)-[0-9]{1,2}","",getNames(tmp1, dim=1))
-  all_mifs <- mbind(all_mifs, tmp1)
-}
-
-meslog(paste0("\n### Generate joint mif, remind2 format: ", filename_remind2_mif), LOGFILE)
-
-write.report(all_mifs, file = filename_remind2_mif)
-
-meslog(paste0("\n### Using mapping, generate joint mif:  ", OUTPUT_mif), LOGFILE)
-
-set_model_and_scenario <- function(mif, model, scen_remove = NULL, scen_add = NULL){
-  dt <- readMIF(mif)
-  scenario_names <- unique(dt$Scenario)
-  dt[, Model := model]
-  if (!is.null(scen_remove)) dt[, Scenario := gsub(scen_remove,"",Scenario)]
-  if (!is.null(scen_add)) {
-    if (grepl(scen_add, unique(dt$Scenario), fixed = TRUE)){
-      meslog(sprintf("Prefix %s already found in scenario name in %s.", scen_add, mif), LOGFILE)
+  ### generate mapping if not specified
+  if (is.null(mapping) || ! all(file.exists(mapping))) {
+    if (all(mapping %in% names(piamInterfaces::templateNames()))) {
+      mappingFile <- file.path(outputFolder, paste0(paste0(c("mapping", if (is.null(project)) mapping else project), collapse = "_"), ".csv"))
     } else {
-      dt[, Scenario := paste0(scen_add,Scenario)]
+      stop("# mapping = '", paste(mapping, collapse = ","), "' not understood. ",
+           "Specify a file or templates such as ", paste0(names(piamInterfaces::templateNames()), collapse = ","))
     }
+    message("\n### Generate ", mappingFile, " based on piamInterfaces templates ", paste(mapping, collapse = ","), ".")
+    mapreturn <- piamInterfaces::generateMappingfile(templates = mapping, outputDirectory = ".",
+                                 fileName = mappingFile, model = model, commentFileName = NULL)
+    mapping <- mappingFile
+    write(paste0("\n\n\n### Comments from piamInterfaces::generateMappingfile\n\n",
+          paste(paste0("- ", mapreturn[["comments"]]$Variable, ": ", mapreturn[["comments"]]$Comment), collapse = "\n"), "\n\n"),
+          file = logFile, append = TRUE)
   }
-  if (length(unique(dt$Scenario)) < length(scenario_names)) {
-    meslog(paste0(length(scenario_names), " scenario names before changes: ", paste(scenario_names, collapse = ", ")), LOGFILE)
-    meslog(paste0(length(unique(dt$Scenario)), " scenario names after changes:  ", paste(unique(dt$Scenario), collapse = ", ")), LOGFILE)
-    stop("Changes to scenario names lead to duplicates. Adapt scen_remove='", scen_remove, "' and scen_add='", scen_add, "'!")
+
+  message("\n### Generating ", OUTPUT_mif, " and .xlsx using mapping ", mapping, ".")
+
+  ### define filenames
+
+  gdxs <- file.path(outputdirs, "fulldata.gdx")
+  scenNames <- getScenNames(outputdirs)
+  mif_path <- file.path(outputdirs, paste0("REMIND_generic_", scenNames, ".mif"))
+  mif_path_polCosts <- file.path(outputdirs, paste0("REMIND_generic_", scenNames, "_adjustedPolicyCosts.mif"))
+  mif_path <- ifelse(file.exists(mif_path_polCosts), mif_path_polCosts, mif_path)
+
+  existingfiles <- file.exists(mif_path)
+  if (! all(existingfiles)) {
+    message("\n### These mif files cannot be found and will be skipped:\n- ",
+            paste0(mif_path[! existingfiles], collapse = "\n- "))
+    mif_path <- mif_path[existingfiles]
+    gdxs <- gdxs[existingfiles]
+    scenNames <- scenNames[existingfiles]
   }
-  writeMIF(dt, mif)
-}
+  message("\n### These mif files are used:\n- ", paste0(mif_path, collapse = "\n- "))
 
-meslog(paste0("# Correct model name to '", MODEL, "'"), LOGFILE)
-meslog(paste0("# Adapt scenario names: '",
-        ADD_TO_SCEN, "' will be prepended, '", REMOVE_FROM_SCEN, "' will be removed."), LOGFILE)
-set_model_and_scenario(filename_remind2_mif, MODEL, REMOVE_FROM_SCEN, ADD_TO_SCEN)
+  filename_remind2_mif <- paste0(outputFilename, "_remind2.mif")
 
-meslog(paste0("# Apply mapping from ", MAPPING), LOGFILE)
+  message("\n### Read mif files...")
 
-iamc::write.reportProject(filename_remind2_mif, MAPPING, OUTPUT_mif, append = FALSE, missing_log = LOGFILE)
+  all_mifs <- NULL
+  for (mif in mif_path) {
+    tmp1 <- read.report(mif, as.list=FALSE)
+    # remove -rem-xx and mag-xx from scenario names
+    getNames(tmp1, dim = 1) <- gsub("-(rem|mag)-[0-9]{1,2}","",getNames(tmp1, dim=1))
+    all_mifs <- mbind(all_mifs, tmp1)
+  }
 
-meslog("# Restore PM2.5 and poverty w.r.t. median income dots in variable names", LOGFILE)
-command <- paste("sed -i 's/wrt median income/w\\.r\\.t\\. median income/g;s/PM2\\_5/PM2\\.5/g'", OUTPUT_mif)
-system(command)
+  message("\n### Generate joint mif, remind2 format: ", filename_remind2_mif)
 
-# message("add two trailing spaces for Policy Cost|GDP Loss|w/o transfers (as in DB)")
-# message("make sure that no other variable contains: w/o transfers")
-# command <- paste("sed -i 's/Policy Cost\\|GDP Loss\\|w\/o transfers/Policy Cost\\|GDP Loss\\|w\\/o transfers  /g; s/w\/o transfers/w\\/o transfers  /g'", OUTPUT_mif)
-# system(command)
+  write.report(all_mifs, file = filename_remind2_mif)
 
-meslog("# Replace N/A for missing years with blanks as recommended by Ed Byers", LOGFILE)
-command <- paste("sed -i 's/N\\/A//g'", OUTPUT_mif)
-system(command)
+  message("\n### Using mapping, generate joint mif:  ", OUTPUT_mif)
 
-meslog("# Remove unwanted timesteps", LOGFILE)
-command <- paste("cut -d ';' -f1-21 ", OUTPUT_mif, " > tmp_reporting.tmp")
-system(command)
-command <- paste("mv tmp_reporting.tmp", OUTPUT_mif)
-system(command)
+  set_model_and_scenario <- function(mif, model, scen_remove = NULL, scen_add = NULL){
+    dt <- readMIF(mif)
+    scenario_names <- unique(dt$Scenario)
+    dt[, Model := model]
+    if (! is.null(scen_remove)) dt[, Scenario := gsub(scen_remove, "", Scenario)]
+    if (! is.null(scen_add)) {
+      if (all(grepl(scen_add, unique(dt$Scenario), fixed = TRUE))) {
+        message(sprintf("Prefix %s already found in all scenario name in %s. Skipping.", scen_add, mif))
+      } else {
+        dt[, Scenario := paste0(scen_add,Scenario)]
+      }
+    }
+    if (length(unique(dt$Scenario)) < length(scenario_names)) {
+      message(length(scenario_names), " scenario names before changes: ", paste(scenario_names, collapse = ", "))
+      message(length(unique(dt$Scenario)), " scenario names after changes:  ", paste(unique(dt$Scenario), collapse = ", "))
+      stop("Changes to scenario names lead to duplicates. Adapt scen_remove='", scen_remove, "' and scen_add='", scen_add, "'!")
+    }
+    writeMIF(dt, mif)
+  }
 
-## HERE you can change the data again, if required
-mifdata <- read.quitte(OUTPUT_mif, factors = FALSE) %>%
-  mutate(model = paste(MODEL)) %>%
-  mutate(value = ifelse(!is.finite(value) | is.na(value), 0, value)) %>%
-  mutate(scenario = gsub("NA", "", scenario))
+  message("# Correct model name to '", model, "'.")
+  message("# Adapt scenario names: '",
+          addToScen, "' will be prepended, '", removeFromScen, "' will be removed.")
+  set_model_and_scenario(filename_remind2_mif, model, removeFromScen, addToScen)
 
-meslog(paste0("# ", length(temporarydelete), " variables are in the list to be temporarily deleted, ",
-        length(unique(mifdata$variable[mifdata$variable %in% temporarydelete])), " were deleted."), LOGFILE)
-write(paste0("  - ", paste(unique(mifdata$variable[mifdata$variable %in% temporarydelete]), collapse = "\n  - "), "\n\n"),
-      file = LOGFILE, append = TRUE)
-mifdata <- filter(mifdata, ! variable %in% temporarydelete)
+  message("# Apply mapping from ", mapping)
 
-if (file.exists(iiasatemplate)) {
-  mifdata <- checkIIASASubmission(mifdata, iiasatemplate, LOGFILE)
-} else {
-  meslog(paste0("# iiasatemplate ", iiasatemplate , " does not exist, returning full list of variables."), logfile)
-}
+  iamc::write.reportProject(filename_remind2_mif, mapping, OUTPUT_mif, append = FALSE, missing_log = logFile)
 
-# check whether all scenarios have same number of variables
-scenarios <- unique(mifdata$variable)
-for (i in 1:length(scenarios)) {
-  stopifnot(length(filter(mifdata, variable %in% scenarios[[1]])) == length(filter(mifdata, variable %in% scenarios[[i]])))
-}
+  message("# Restore PM2.5 and poverty w.r.t. median income dots in variable names")
+  command <- paste("sed -i 's/wrt median income/w\\.r\\.t\\. median income/g;s/PM2\\_5/PM2\\.5/g'", OUTPUT_mif)
+  system(command)
 
-file.remove(OUTPUT_mif)
-write.mif(mifdata, OUTPUT_mif)
+  # message("add two trailing spaces for Policy Cost|GDP Loss|w/o transfers (as in DB)")
+  # message("make sure that no other variable contains: w/o transfers")
+  # command <- paste("sed -i 's/Policy Cost\\|GDP Loss\\|w\/o transfers/Policy Cost\\|GDP Loss\\|w\\/o transfers  /g; s/w\/o transfers/w\\/o transfers  /g'", OUTPUT_mif)
+  # system(command)
 
-# filter(scenario == "d_delfrag") %>%
+  message("# Replace N/A for missing years with blanks as recommended by Ed Byers")
+  command <- paste("sed -i 's/N\\/A//g'", OUTPUT_mif)
+  system(command)
 
-mifdata %>%
-  mutate(value = ifelse(!is.finite(value) | paste(value) == "", 0, value)) %>%
-  pivot_wider(names_from="period", values_from="value") -> writetoexcel
+  message("# Remove unwanted timesteps")
+  command <- paste("cut -d ';' -f1-21 ", OUTPUT_mif, " > tmp_reporting.tmp")
+  system(command)
+  command <- paste("mv tmp_reporting.tmp", OUTPUT_mif)
+  system(command)
 
-writexl::write_xlsx(list("data" = writetoexcel), OUTPUT_xlsx)
+  ## HERE you can change the data again, if required
+  mifdata <- read.quitte(OUTPUT_mif, factors = FALSE) %>%
+    mutate(model = paste(model)) %>%
+    mutate(value = ifelse(!is.finite(value) | is.na(value), 0, value)) %>%
+    mutate(scenario = gsub("NA", "", scenario))
 
-meslog(paste0("\n### mif files: ", sum(existingfiles), " used, ", sum(! existingfiles), " not found, see above."), LOGFILE)
-meslog(paste0("\n### Output files written:\n- ", OUTPUT_mif, "\n- ", OUTPUT_xlsx), LOGFILE)
-message("\n### See log file: less ", LOGFILE, "\n")
+  message("# ", length(temporarydelete), " variables are in the list to be temporarily deleted, ",
+          length(unique(mifdata$variable[mifdata$variable %in% temporarydelete])), " were deleted.")
+  write(paste0("  - ", paste(unique(mifdata$variable[mifdata$variable %in% temporarydelete]), collapse = "\n  - "), "\n\n"),
+        file = logFile, append = TRUE)
+  mifdata <- filter(mifdata, ! variable %in% temporarydelete)
+
+  if (file.exists(iiasatemplate)) {
+    mifdata <- checkIIASASubmission(mifdata, iiasatemplate, logFile)
+  } else {
+    message("# iiasatemplate ", iiasatemplate , " does not exist, returning full list of variables.")
+  }
+
+  # check whether all scenarios have same number of variables
+  scenarios <- unique(mifdata$variable)
+  for (i in 1:length(scenarios)) {
+    stopifnot(length(filter(mifdata, variable %in% scenarios[[1]])) == length(filter(mifdata, variable %in% scenarios[[i]])))
+  }
+
+  file.remove(OUTPUT_mif)
+  write.mif(mifdata, OUTPUT_mif)
+
+  # perform summation checks
+  checkSummationData <- checkSummations(OUTPUT_mif, template = mapping, summationsFile = "AR6",
+                                        logFile = logFile, logAppend = TRUE,
+                                        dataDumpFile = file.path(outputFolder, "checkSummations.csv"))
+
+  mifdata %>%
+    mutate(value = ifelse(!is.finite(value) | paste(value) == "", 0, value)) %>%
+    pivot_wider(names_from="period", values_from="value") -> writetoexcel
+
+  writexl::write_xlsx(list("data" = writetoexcel), OUTPUT_xlsx)
+
+  message("\n### mif files: ", sum(existingfiles), " used, ", sum(! existingfiles), " not found, see above.")
+  message("\n### Output files written:\n- ", OUTPUT_mif, "\n- ", OUTPUT_xlsx)
+
+}, message = function(x) {
+  cat(x$message, file = logFile, append = TRUE)
+}) # end of piping message to logfile
+
+message("\n### See details log file: less ", logFile, "\n")
---
+++ tutorials/13_submit_to_IIASA_database.md
@@ -0,0 +1,43 @@
+Uploading scenario data to IIASA database
+================
+Oliver Richters, 24 October, 2022
+
+Many projects have to upload their scenario data to the database provided by [IIASA](https://iiasa.ac.at/scenario-ensembles-and-database-resources).
+
+=== Step 1: model registration ===
+
+At the beginning of the project, there should be a process to get access to the project Internal Scenario Explorer.
+
+Model, scenarios and project variables should be registered in the IIASA database. The variable list can be based on the `AR6` template once generate for the IPCC Sixth Assessment Report, or the new template for the NAVIGATE project.
+
+In case of problems, contact [Daniel Huppmann](https://iiasa.ac.at/staff/daniel-huppmann). A template file containing the list of variables and associated units should be provided as yaml or xlsx file. Save it in your REMIND repository, the suggested place is `./output/export/`.
+
+=== Step 2: generate file to upload ===
+
+The script that generates the xlsx file required for a submission is [`scripts/output/export/xlsx_IIASA.R`](../scripts/output/export/xlsx_IIASA.R). It is called by running `./output.R`, then selecting `export` and `xlsx_IIASA`. Then select the directories of the runs you would like to use.
+
+The script requires the following project-specific inputs:
+- model: model name as registered in the database, such as "REMIND-MAgPIE 3.0-4.4"
+- mapping: either the path to a mapping file you generated using [`piamInterfaces::generateMappingfile`](https://github.com/pik-piam/piamInterfaces/blob/master/R/generateMappingfile.R), of a vector of templates such as `c("NAVIGATE", "SHAPE")` referring to the last part of the file names in [this piamInterfaces directory](https://github.com/pik-piam/piamInterfaces/tree/master/inst/templates).
+- iiasatemplate: path to the file with variables and units obtained in the project
+- addToScen: optional string added in front of all scenario names
+- removeFromScen: optional regular expression of parts to be deleted from the scenario names  <- "C_|_bIT|_bit|_bIt"
+- filename_prefix: optional prefix of the resulting outputFile, such as your project name
+
+You can specify the information above in two ways: Either edit [`xlsx_IIASA.R`](../scripts/output/export/xlsx_IIASA.R) and add a project in a similar way to `NGFS_v3` or `ENGAGE_4p5`. You can then start the scripts with (you don't need to specify `comp` and `output`, you can just wait to be asked for it):
+```
+Rscript output.R comp=export output=xlsx_IIASA project=NGFS_v3 
+```
+An alternative is to specify everything individually as command-line arguments:
+```
+Rscript output.R comp=export output=xlsx_IIASA model="REMIND 3.0" mapping=AR6,AR6_NGFS addToScen=whatever removeFromScen=C_ filename_prefix=test
+```
+All the information printed to you during the run will also be present in the logfile whose path will be told you at the end.
+
+=== Step 3: check submission ===
+Check the logfile carefully for the variables that were omitted, failing summation checks etc.
+
+In the past, IIASA has sometimes changed unit names to correct spelling mistakes or harmonize them. If there were unit mismatches where the units are identical, just spelled differently, you can add them to the named vector `identicalUnits` in [`piamInterfaces::checkIIASASubmission`](https://github.com/pik-piam/piamInterfaces/blob/master/R/checkIIASASubmission.R). So if the project template expects `Mt/yr`, but our templates export it as `Mt/year`, add `"Mt/yr" = "Mt/year"` to the vector, and it will in the future not fail on this unit mismatch but correct it to what is required in the project. Never use this mechanism if the units are not actually identical in their meaning.
+
+=== Step 4: upload file ===
+Go to the project internal Scenario Explorer, click on your login name and then on "uploads" and the "plus" in the upper right corner - submit your xlsx file. Don't expect it to work flawlessly on the first try so hope for the best. You will receive an email message with a log and may at some point need the help of the IIASA administrators of your project.
