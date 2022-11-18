bbf5d4ead205fe4b6af90fb50ae371fef3075c06
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-11-18 09:27:38 +0100

    check piamenv available instead of lucode2

--- start.R
+++ start.R
@@ -219,11 +219,11 @@ if (any(c("--testOneRegi", "--debug", "--quick") %in% flags) & "--restart" %in% 
 }
 
 # Check if dependencies for a model run are fulfilled
-if (packageVersion("lucode2") >= "0.34.0") {
+if (packageVersion("piamenv") >= "0.2.0") {
   piamenv::checkDeps(action = "ask")
 } else {
-  stop("REMIND requires lucode2 >= 0.34.0, please run the following to update it:\n",
-       "renv::install('lucode2'); renv::snapshot(prompt = FALSE)\n",
+  stop("REMIND requires piamenv >= 0.2.0, please run the following to update it:\n",
+       "renv::install('piamenv'); renv::snapshot(prompt = FALSE)\n",
        "and re-run start.R in a fresh R session.")
 }
 
