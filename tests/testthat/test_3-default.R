72401a24557729306f1268b2a5791a63e65cb3c1
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-11-29 15:12:33 +0100

    Add slurm config for slow tests, move test configs in subdirectory.

--- tests/testthat/test_3-default.R
+++ tests/testthat/test_3-default.R
@@ -1,5 +1,5 @@
 test_that("start.R works", {
   skip_if_fast()
-  status <- local_system2("Rscript", c("start.R"))
+  status <- local_system2("Rscript", c("start.R", "config/tests/scenario_config_default.csv"))
   expect_equal(status, 0)
 })
