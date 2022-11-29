e93cf7f6e24ea4adc6f4d7dac3746f8e3f322cdf
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-11-29 15:43:41 +0100

    Correct documentation.

--- tests/testthat/test_1-quick.R
+++ tests/testthat/test_1-quick.R
@@ -1,4 +1,4 @@
-test_that("start.R config/scenario_config_quick.csv works", {
+test_that("start.R config/tests/scenario_config_quick.csv works", {
   status <- local_system2("Rscript", c("start.R", "config/tests/scenario_config_quick.csv"))
   expect_equal(status, 0)
 })
