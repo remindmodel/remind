b55d4f0cd28a945a97b4b84b0e1f1b95772f766e
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2023-01-11 14:04:54 +0100

    Proper coupled config in coupled tests.

--- tests/testthat/test_20-coupled.R
+++ tests/testthat/test_20-coupled.R
@@ -10,7 +10,7 @@ test_that("environment is suitable for coupled tests", {
 test_that("runs coupled to MAgPIE work", {
   skipIfFast()
   skipIfPreviousFailed()
-  output <- localSystem2("Rscript", c("start_bundle_coupled.R", "config/tests/scenario_config_shortCascade.csv"),
+  output <- localSystem2("Rscript", c("start_bundle_coupled.R", "config/tests/scenario_config_coupled_shortCascade.csv"),
                          env = "R_PROFILE_USER=.snapshot.Rprofile")
   printIfFailed(output)
   expectSuccessStatus(output)
