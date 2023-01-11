e02958ae760fda60626a023501d81608d5158a17
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2023-01-11 11:32:14 +0100

    Add coupling test.

---
+++ tests/testthat/test_20-coupled.R
@@ -0,0 +1,17 @@
+test_that("environment is suitable for coupled tests", {
+  skipIfFast()
+  skipIfPreviousFailed()
+  # magpie needs to be cloned by the user before running coupled tests
+  expect_true(dir.exists("../magpie"))
+  # coupled tests need slurm
+  exepct_true(isSlurmAvailable())
+})
+
+test_that("runs coupled to MAgPIE work", {
+  skipIfFast()
+  skipIfPreviousFailed()
+  output <- localSystem2("Rscript", c("start_bundle_coupled.R", "config/tests/scenario_config_shortCascade.csv"),
+                         env = "R_PROFILE_USER=.snapshot.Rprofile")
+  printIfFailed(output)
+  expectSuccessStatus(output)
+})
