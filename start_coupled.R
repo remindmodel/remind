255e6856f915d8064fa318b531eb5e819da183f0
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2023-01-12 18:35:40 +0100

    Better calculation of minimally needed output in coupled remind.

--- start_coupled.R
+++ start_coupled.R
@@ -118,7 +118,7 @@ start_coupled <- function(path_remind, path_magpie, cfg_rem, cfg_mag, runname, m
     
     # Switch off generation of needless output for all but the last REMIND iteration
     if (i < max_iterations) {
-      cfg_rem$output <- c("reporting", "emulator", "rds_report")
+      cfg_rem$output <- intersect(c("reporting", "emulator", "rds_report", "reportingREMIND2MAgPIE"), cfg_rem_original)
     } else {
       cfg_rem$output <- cfg_rem_original
     }
