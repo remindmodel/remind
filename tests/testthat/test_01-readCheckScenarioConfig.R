09545375de5c28b187d84019399007305e8f6a28
orichters <oliverr@pik-potsdam.de>
2023-02-16 11:43:27 +0100

    move copyFromConfig to own function, add tests, undo NGFS changes

--- tests/testthat/test_01-readCheckScenarioConfig.R
+++ tests/testthat/test_01-readCheckScenarioConfig.R
@@ -14,9 +14,10 @@ test_that("readCheckScenarioConfig fails on error-loaden config", {
   writeLines(c(";start;copyConfigFrom;c_budgetCO2;path_gdx;path_gdx_carbonprice",
                "abc.loremipsumloremipsum@lorem&ipsumloremipsumloremipsumloremipsumloremipsumloremipsum_;0;;33;;",
                "PBS;1;glob;29; whitespacebefore;whitespaceafter ",
-               "glob;0;missing_copyConfigFrom;33; ;nobreakspace	tab"),
+               "glob;0;missing_copyConfigFrom;33; ;nobreakspace	tab",
+               "PBScopy;0;PBS;;;mustbedifferenttoPBS"),
              con = csvfile, sep = "\n")
-  w <- capture_warnings(readCheckScenarioConfig(csvfile, remindPath = "../../", testmode = TRUE))
+  w <- capture_warnings(scenConf <- readCheckScenarioConfig(csvfile, remindPath = "../../", testmode = TRUE))
   expect_match(w, "11 errors found", all = FALSE)
   expect_match(w, "These titles are too long", all = FALSE)
   expect_match(w, "These titles may be confused with regions", all = FALSE)
@@ -24,6 +25,11 @@ test_that("readCheckScenarioConfig fails on error-loaden config", {
   expect_match(w, "\\.@&", all = FALSE)
   expect_match(w, "Outdated column names found that must not be used", all = FALSE)
   expect_match(w, "contain whitespaces", all = FALSE)
-  expect_match(w, "following scenario names indicated in copyConfigFrom column were not found", all = FALSE)
-  expect_match(w, "following scenarios references in copyConfigFrom column a scenario defined only later", all = FALSE)
+  expect_match(w, "scenario names indicated in copyConfigFrom column were not found", all = FALSE)
+  expect_match(w, "specify in copyConfigFrom column a scenario name defined below in the file", all = FALSE)
+  copiedFromPBS <- c("c_budgetCO2", "path_gdx", "path_gdx_ref")
+  expect_identical(unlist(scenConf["PBS", copiedFromPBS]),
+                   unlist(scenConf["PBScopy", copiedFromPBS]))
+  expect_identical(scenConf["PBScopy", "path_gdx_carbonprice"], "mustbedifferenttoPBS")
+  expect_identical(scenConf["PBS", "path_gdx_carbonprice"], "whitespaceafter ")
 })
