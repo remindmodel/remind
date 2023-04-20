bab5e70bc7745aa9677987d507aafefe136a25a6
Lavinia Baumstark <baumstark@pik-potsdam.de>
2023-04-20 15:36:52 +0200

    update headers

--- tests/testthat/test_01-addTitletag.R
+++ tests/testthat/test_01-addTitletag.R
@@ -1,3 +1,9 @@
+# |  (C) 2006-2023 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of REMIND and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  REMIND License Exception, version 1.0 (see LICENSE file).
+# |  Contact: remind@pik-potsdam.de
 test_that("addTitletag works", {
   start <- c("0", "1", "1,test", "test")
   path_gdx_ref <- c("some.gdx", "A", "C", "other.gdx")
