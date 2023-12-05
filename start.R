594929fa614bdb4cebcc9992ec987e7095f60b9a
Falk Benke <falk.benke@pik-potsdam.de>
2023-12-05 16:36:44 +0100

    test

--- start.R
+++ start.R
@@ -53,6 +53,8 @@ helpText <- "
 #' settings.
 "
 
+Foo
+
 # Source everything from scripts/start so that all functions are available everywhere
 invisible(sapply(list.files("scripts/start", pattern = "\\.R$", full.names = TRUE), source))
 
