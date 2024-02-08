97b7aa101ed17fff0b278600d11f6ae436ab46b9
Tonn Rüter <tonnru@pik-potsdam.de>
2024-02-08 15:23:17 +0100

    Reduce slurm option selection when running ar6 reporting

--- output.R
+++ output.R
@@ -87,9 +87,12 @@ choose_slurmConfig_output <- function(output) {
   slurm_options <- c("--qos=priority", "--qos=short", "--qos=standby",
                      "--qos=priority --mem=8000", "--qos=short --mem=8000",
                      "--qos=standby --mem=8000", "--qos=priority --mem=32000",
-                     if ("ar6Climate" %in% output) "--qos=priority --tasks-per-node=12 --mem=32000",
                      "direct")
   if ("reporting" %in% output) slurm_options <- unique(c(grep("--mem=[0-9]*[0-9]{3}", slurm_options, value = TRUE), "direct"))
+  # Modify slurm options for ar6 reporting, since we want to run MAGICC in parallel and we'll need a lot of memory
+  if ("ar6Climate" %in% output) slurm_options <- c("--qos=priority --tasks-per-node=12 --mem=32000",
+                                                   "--qos=short --tasks-per-node=12 --mem=32000",
+                                                   "--qos=standby --tasks-per-node=12 --mem=32000")
   if (length(slurm_options) == 1) {
     return(slurm_options[[1]])
   }
