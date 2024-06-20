1346cb68076a9ea2435434d81238773bcf396583
David Klein <dklein@pik-potsdam.de>
2024-06-20 11:36:40 +0200

    Use the folder from which the R session is started as the renv project (not always the main folder). Change renv test back from warning into error.

--- scripts/start/run.R
+++ scripts/start/run.R
@@ -259,7 +259,7 @@ run <- function() {
   # make sure the renv used for the run is also used for generating output
   if (!is.null(renv::project())) {
     if (normalizePath(renv::project()) != normalizePath(outputdir)) {
-      warning("loaded renv=", normalizePath(renv::project()), " and outputdir=", normalizePath(outputdir), " must be equal.")
+      stop("loaded renv=", normalizePath(renv::project()), " and outputdir=", normalizePath(outputdir), " must be equal.")
     }
     argv <- c(get0("argv"), paste0("--renv=", renv::project()))
   }
