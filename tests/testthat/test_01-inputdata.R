b79db53b1e03a545939e5ee4a6f4168fe4e00e81
David Klein <dklein@pik-potsdam.de>
2024-10-10 14:36:13 +0200

    Use capture_warnings instead of capture_warning, the latter of which wouldnt let updateInputData finish properly

--- tests/testthat/test_01-inputdata.R
+++ tests/testthat/test_01-inputdata.R
@@ -8,10 +8,10 @@ test_that("Are all input data files present?", {
   missinginput <- missingInputData(path = "../..")
   if (length(missinginput) > 0) {
     lockID <- gms::model_lock(folder = "../..")
-    w <- capture_warning(updateInputData(cfg = gms::readDefaultConfig("../.."), remindPath = "../.."))
+    w <- capture_warnings(updateInputData(cfg = gms::readDefaultConfig("../.."), remindPath = "../.."))
     # ignore warning about missing historical.mif, raise warning if there were other wargnings
     ignore <- "File historical.mif seems to be missing!"
-    if (!w$message %in% ignore) {
+    if (!w %in% ignore) {
       warning(paste0("'updateInputData' raised the following wargnings\n", paste(w, collapse = "\n")))
     }
     gms::model_unlock(lockID)
