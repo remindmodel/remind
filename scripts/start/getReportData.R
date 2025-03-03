7c426086aacc15f812b74ce6aad1617499ec3c41
David Klein <dklein@pik-potsdam.de>
2025-03-03 14:30:48 +0100

    Add new land use emissions sub types. Start transferring all functions in getReportData.R from magclass to quitte / tibble.

--- scripts/start/getReportData.R
+++ scripts/start/getReportData.R
@@ -8,6 +8,9 @@
 getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc="costs",var_luc="smooth") {
   
   require(magclass, quietly = TRUE,warn.conflicts =FALSE)
+  require(dplyr,    quietly = TRUE,warn.conflicts =FALSE)
+  require(quitte,   quietly = TRUE,warn.conflicts =FALSE)
+  require(readr,    quietly = TRUE,warn.conflicts =FALSE)
   
   .bioenergy_price <- function(mag){
     notGLO <- getRegions(mag)[!(getRegions(mag)=="GLO")]
@@ -23,6 +26,32 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
     dimnames(out)[[3]] <- NULL #Delete variable name to prevent it from being written into output file
     write.magpie(out[notGLO,,],paste0("./modules/30_biomass/",inputpath_mag,"/input/p30_pebiolc_pricemag_coupling.csv"),file_type="csvr")
     return(tmp[notGLO,,])
+  }
+  
+  .bioenergy_price_new <- function(mag, path_to_report, inputpath_mag) {
+    
+    mag2rem <- tribble(
+      ~mag,                                ~factorMag2Rem,
+      "Prices|Bioenergy", 0.0315576
+    )
+    
+    filename <- paste0("./modules/30_biomass/",inputpath_mag,"/input/p30_pebiolc_pricemag_coupling_new.csv")
+    
+    mag <- as.quitte(mag)
+    
+    rem <- mag |>
+      inner_join(mag2rem, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
+      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
+      mutate(value = round(value, digits = 11))          |> # limit number of decimals
+      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
+      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
+      select(period, region, value)                      |> # keep relevant columns only
+      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
+      readr::write_csv(file = filename, col_names = TRUE)
+    
+    write(paste0("*** EOF ", filename ," ***"), file = filename, append = TRUE)
+    
+    return(rem)
   }
 
   .bioenergy_production <- function(mag){
@@ -126,6 +155,75 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
     return(out[notGLO,,])
   }
   
+  .emissions <- function(mag, var_luc, path_to_report) {
+
+    # define three columns of dataframe:
+    #   emimag (magpie emission names)
+    #   emirem (remind emission names)
+    #   factorMag2Rem (factor for converting magpie to remind emissions)
+    #   1/1000*28/44, # kt N2O/yr -> Mt N2O/yr -> Mt N/yr
+    #   28/44,        # Tg N2O/yr =  Mt N2O/yr -> Mt N/yr
+    #   1/1000*12/44, # Mt CO2/yr -> Gt CO2/yr -> Gt C/yr
+
+    mag2rem <- tribble(
+      ~emimag,                                                                          ~emirem,                       ~factorMag2Rem,
+      "Emissions|CO2|Land|+|Land-use Change                      "                      , "co2luc"                  ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|CO2-price AR"                      , "co2lucCDRintentByPrice"  ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|NPI_NDC AR"                        , "co2lucCDRintentByReg"    ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Cropland Tree Cover"               , "co2lucCDRintentCropland" ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Timber|+|Storage in HWP"                      , "co2lucCDRintentTimber"   ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Other Land"                        , "co2lucCDRunintent"       ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Secondary Forest"                  , "co2lucCDRunintent"       ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Regrowth|+|Timber Plantations"                , "co2lucCDRunintent"       ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|SOM|+|Withdrawals"                            , "co2lucCDRunintent"       ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|+|Deforestation"                              , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|+|Other land conversion"                      , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|+|Peatland"                                   , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|+|Wood Harvest"                               , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|SOM|+|Emissions"                              , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|Timber|+|Release from HWP"                    , "co2lucPositive"          ,   1/1000*12/44,
+      "Emissions|CO2|Land|Land-use Change|+|Residual"                                   , "co2lucResidual"          ,   1/1000*12/44,
+      "Emissions|N2O|Land|Agriculture|+|Animal Waste Management"                        , "n2oanwstm"               ,   28/44,
+      "Emissions|N2O|Land|Agriculture|Agricultural Soils|+|Inorganic Fertilizers"       , "n2ofertin"               ,   28/44,
+      "Emissions|N2O|Land|Agriculture|Agricultural Soils|+|Manure applied to Croplands" , "n2oanwstc"               ,   28/44,
+      "Emissions|N2O|Land|Agriculture|Agricultural Soils|+|Decay of Crop Residues"      , "n2ofertcr"               ,   28/44,
+      "Emissions|N2O|Land|Agriculture|Agricultural Soils|+|Soil Organic Matter Loss"    , "n2ofertsom"              ,   28/44,
+      "Emissions|N2O|Land|Agriculture|Agricultural Soils|+|Pasture"                     , "n2oanwstp"               ,   28/44,
+      "Emissions|N2O|Land|+|Peatland"                                                   , "n2opeatland"             ,   28/44,
+      "Emissions|CH4|Land|Agriculture|+|Rice"                                           , "ch4rice"                 ,   1,
+      "Emissions|CH4|Land|Agriculture|+|Animal waste management"                        , "ch4anmlwst"              ,   1,
+      "Emissions|CH4|Land|Agriculture|+|Enteric fermentation"                           , "ch4animals"              ,   1,
+      "Emissions|CH4|Land|+|Peatland"                                                   , "ch4peatland"             ,   1
+    )
+
+    if (var_luc == "smooth") {
+      # do nothing and use variable names as defined above
+    } else if (var_luc == "raw") {
+      # add RAW to variable names
+      mag2rem$emimag <- gsub("Emissions|CO2|Land","Emissions|CO2|Land RAW", mag2rem$emimag, fixed = TRUE)
+    } else {
+      stop(paste0("Unkown setting for 'var_luc': `", var_luc, "`. Only `smooth` or `raw` are allowed."))
+    }
+
+    filename <- "./core/input/f_macBaseMagpie_coupling_new.cs4r"
+    
+    mag <- as.quitte(mag)
+
+    rem <- mag |>
+      inner_join(mag2rem, by = c("variable" = "emimag")) |> # combine tables keeping relevant variables only
+      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
+      group_by(period, region, emirem)                   |> # define groups for summation
+      summarise(value = sum(value))                      |> # sum MAgPIE emissions (emimag) that have the same enty in remind (emirem)
+      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
+      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
+      readr::write_csv(file = filename, col_names = FALSE)
+    
+    write(paste0("*** EOF ", filename ," ***"), file = filename, append = TRUE)
+    
+    return(rem)
+      
+  }
+  
   .agriculture_costs <- function(mag){
     notGLO <- getRegions(mag)[!(getRegions(mag)=="GLO")]
     out <- mag[,,"Costs Without Incentives (million US$2017/yr)"]/1000/1000 # with transformation factor from 10E6 US$2017 to 10E12 US$2017
@@ -150,11 +248,13 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
   years <- 2000+5*(1:30)
   mag <- time_interpolate(rep,years)
   pricBio <- .bioenergy_price(mag)
-  prodBio <- .bioenergy_production(mag)
+  pricBio_new <- .bioenergy_price_new(mag, path_to_report, inputpath_mag)
+  #prodBio <- .bioenergy_production(mag)
   emi     <- .emissions_mac(mag)
-  cost    <- .agriculture_costs(mag)
-  tmp <- mbind(pricBio, prodBio, emi, cost)
+  emi_new <- .emissions(mag, var_luc, path_to_report)
+  #cost    <- .agriculture_costs(mag)
+  #tmp <- mbind(pricBio, prodBio, emi, cost)
   # needs to be updated to MAgPIE 4 interface
   #.agriculture_tradebal(mag)
-  return(invisible(tmp))
+  #return(invisible(tmp))
 }
