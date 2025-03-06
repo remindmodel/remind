d637c14a2f2339fcc90bc4376dfb0c6d585332f3
David Klein <dklein@pik-potsdam.de>
2025-03-06 17:54:04 +0100

    Use internal function that holds the dplyr commands

--- scripts/start/getReportData.R
+++ scripts/start/getReportData.R
@@ -11,59 +11,10 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
   require(quitte,   quietly = TRUE,warn.conflicts =FALSE)
   require(readr,    quietly = TRUE,warn.conflicts =FALSE)
   
-  .bioenergy_price <- function(mag, file, path_to_report) {
-    
-    mag2rem <- tribble(
-      ~mag,              ~factorMag2Rem,
-      "Prices|Bioenergy", 0.0315576 # US$2017/GJ to US$2017/Wa
-    )
-
-    rem <- mag |>
-      inner_join(mag2rem, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
-      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
-      mutate(value = round(value, digits = 11))          |> # limit number of decimals
-      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
-      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
-      select(period, region, value)                      |> # keep relevant columns only
-      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
-      readr::write_csv(file = file, col_names = TRUE)
-    
-    # in the old function NAs used to be filtered. Let's try without and re-introduce if occurring again.
-    # out["JPN",is.na(out["JPN",,]),] <- 0
-    
-    write(paste0("*** EOF ", file ," ***"), file = file, append = TRUE)
-    #tidyr::pivot_longer(rem, 2:14, names_to = "region")
-    return(rem)
-  }
-
-  .bioenergy_production <- function(mag, file, path_to_report) {
-    
-    mag2rem <- tribble(
-      ~mag,                                                ~factorMag2Rem,
-      "Demand|Bioenergy|2nd generation|++|Bioenergy crops", 1/31.536 # EJ to TWa
-    )
-    
-    rem <- mag |>
-      inner_join(mag2rem, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
-      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
-      mutate(value = round(value, digits = 11))          |> # limit number of decimals
-      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
-      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
-      select(period, region, value)                      |> # keep relevant columns only
-      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
-      readr::write_csv(file = file, col_names = TRUE)
-    
-    # in the old function NAs and negative values used to be filtered. Let's try without and re-introduce if occurring again.
-    # out["JPN",is.na(out["JPN",,]),] <- 0
-    # out[which(out<0)] <- 0 # set negative values to zero since they cause errors in GMAS power function
-    
-    write(paste0("*** EOF ", file ," ***"), file = file, append = TRUE)
-    
-    return(rem)
-  }
+  magQuitte <- quitte::read.quitte(path_to_report, check.duplicates = FALSE)
   
   .emissions <- function(mag, file, var_luc, path_to_report) {
-
+    
     # define three columns of dataframe:
     #   emimag (magpie emission names)
     #   emirem (remind emission names)
@@ -71,7 +22,7 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
     #   1/1000*28/44, # kt N2O/yr -> Mt N2O/yr -> Mt N/yr
     #   28/44,        # Tg N2O/yr =  Mt N2O/yr -> Mt N/yr
     #   1/1000*12/44, # Mt CO2/yr -> Gt CO2/yr -> Gt C/yr
-
+    
     mag2rem <- tribble(
       ~emimag,                                                                          ~emirem,                       ~factorMag2Rem,
       "Emissions|CO2|Land|+|Land-use Change                      "                      , "co2luc"                  ,   1/1000*12/44,
@@ -102,7 +53,7 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
       "Emissions|CH4|Land|Agriculture|+|Enteric fermentation"                           , "ch4animals"              ,   1,
       "Emissions|CH4|Land|+|Peatland"                                                   , "ch4peatland"             ,   1
     )
-
+    
     if (var_luc == "smooth") {
       # do nothing and use variable names as defined above
     } else if (var_luc == "raw") {
@@ -111,7 +62,7 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
     } else {
       stop(paste0("Unkown setting for 'var_luc': `", var_luc, "`. Only `smooth` or `raw` are allowed."))
     }
-
+    
     rem <- mag |>
       inner_join(mag2rem, by = c("variable" = "emimag")) |> # combine tables keeping relevant variables only
       mutate(value = value * factorMag2Rem)              |> # apply unit conversion
@@ -125,66 +76,62 @@ getReportData <- function(path_to_report,inputpath_mag="magpie_40",inputpath_acc
     
     return(rem)
   }
+
+  .convertAndWrite <- function(mag, mapping, file, path_to_report, format = "long") {
+    
+    mag <- mag |>
+      inner_join(mapping, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
+      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
+      mutate(value = round(value, digits = 11))          |> # limit number of decimals
+      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
+      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
+      select(period, region, value)                      |> # keep relevant columns only
+      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
+      readr::write_csv(file = file, col_names = TRUE)
+
+    write(paste0("*** EOF ", file ," ***"), file = file, append = TRUE)
+    return(mag)
+  }
   
-  .agriculture_costs <- function(mag, file, path_to_report) {
-    
-    mag2rem <- tribble(
-      ~mag,                      ~factorMag2Rem,
-      "Costs Without Incentives", 1/1000/1000 # 10E6 US$2017 to 10E12 US$2017
-    )
-    
-    rem <- mag |>
-      inner_join(mag2rem, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
-      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
-      mutate(value = round(value, digits = 11))          |> # limit number of decimals
-      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
-      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
-      select(period, region, value)                      |> # keep relevant columns only
-      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
-      readr::write_csv(file = file, col_names = TRUE)
-    
-    # in the old function NAs used to be filtered. Let's try without and re-introduce if occurring again.
-    # out["JPN",is.na(out["JPN",,]),] <- 0
+  ### ---- Emissions ----
+  
+  emi      <- .emissions(magQuitte, file = paste0("./core/input/f_macBaseMagpie_coupling_new.cs4r"), var_luc, path_to_report)
+  
+  ### ---- Bioenergy prices ----
+  
+  mag2rem <- tribble(
+    ~mag,              ~factorMag2Rem,
+    "Prices|Bioenergy", 0.0315576) # US$2017/GJ to US$2017/Wa
 
-    write(paste0("*** EOF ", file ," ***"), file = file, append = TRUE)
+  pricBio  <- .convertAndWrite(magQuitte, mag2rem, file = paste0("./modules/30_biomass/",inputpath_mag,"/input/p30_pebiolc_pricemag_coupling_new.csv"), path_to_report)
+  
+  ### ---- Bioenergy production ----
+  
+  mag2rem <- tribble(
+    ~mag,                                                ~factorMag2Rem,
+    "Demand|Bioenergy|2nd generation|++|Bioenergy crops", 1/31.536) # EJ to TWa
     
-    return(rem)
-  }  
-    
-  .agriculture_tradebal <- function(mag, file, path_to_report) {
-    
-    mag2rem <- tribble(
-      ~mag,                      ~factorMag2Rem,
-      "Costs Accounting|+|Trade", 1/1000/1000 # 10E6 US$2017 to 10E12 US$2017
-    )
-    
-    rem <- mag |>
-      inner_join(mag2rem, by = c("variable" = "mag"))    |> # combine tables keeping relevant variables only
-      mutate(value = value * factorMag2Rem)              |> # apply unit conversion
-      mutate(value = round(value, digits = 11))          |> # limit number of decimals
-      relocate(period, .before = region)                 |> # put period in front of region for proper order for GAMS import
-      filter(period >= 2005, region != "World")          |> # keep REMIND time horizon and remove World region
-      select(period, region, value)                      |> # keep relevant columns only
-      tidyr::pivot_wider(names_from = region, values_from = value) |> # make 2D-table
-      readr::write_csv(file = file, col_names = TRUE)
-    
-    # in the old function NAs used to be filtered. Let's try without and re-introduce if occurring again.
-    # out["JPN",is.na(out["JPN",,]),] <- 0
-    
-    write(paste0("*** EOF ", file ," ***"), file = file, append = TRUE)
-    
-    return(rem)
-  }  
+  prodBio  <- .convertAndWrite(magQuitte, mag2rem, file = paste0("./modules/30_biomass/",inputpath_mag,"/input/pm_pebiolc_demandmag_coupling_new.csv"), path_to_report)
 
-  magQuitte <- quitte::read.quitte(path_to_report, check.duplicates = FALSE)
+  ### ---- Agricultural costs ----
 
-  pricBio  <- .bioenergy_price(     magQuitte, file = paste0("./modules/30_biomass/",inputpath_mag,"/input/p30_pebiolc_pricemag_coupling_new.csv"), path_to_report)
-  prodBio  <- .bioenergy_production(magQuitte, file = paste0("./modules/30_biomass/",inputpath_mag,"/input/pm_pebiolc_demandmag_coupling_new.csv"))
-  cost     <- .agriculture_costs(   magQuitte, file = paste0("./modules/26_agCosts/",inputpath_acc,"/input/p26_totLUcost_coupling_new.csv"))
-  emi      <- .emissions(           magQuitte, file = paste0("./core/input/f_macBaseMagpie_coupling_new.cs4r"), var_luc, path_to_report)
+  mag2rem <- tribble(
+    ~mag,                      ~factorMag2Rem,
+    "Costs Without Incentives", 1/1000/1000) # 10E6 US$2017 to 10E12 US$2017
+
+  cost     <- .convertAndWrite(magQuitte, mag2rem, file = paste0("./modules/26_agCosts/",inputpath_acc,"/input/p26_totLUcost_coupling_new.csv"), path_to_report)
+  
+  ### ---- Agricultural trade ----
+  
+  mag2rem <- tribble(
+    ~mag,                      ~factorMag2Rem,
+    "Costs Accounting|+|Trade", 1/1000/1000) # 10E6 US$2017 to 10E12 US$2017
+  
   # needs to be updated to MAgPIE 4 interface
-  # trade <- .agriculture_tradebal(magQuitte, file = paste0("./modules/26_agCosts/",inputpath_acc,"/input/trade_bal_reg.rem.csv"), path_to_report)
+  trade <- .convertAndWrite(magQuitte, mag2rem, file = paste0("./modules/26_agCosts/",inputpath_acc,"/input/trade_bal_reg.rem.csv"), path_to_report)
   
+  ### ---- return ----
+
   #tmp <- mbind(pricBio, prodBio, emi, cost)
   #return(invisible(tmp))
 }
