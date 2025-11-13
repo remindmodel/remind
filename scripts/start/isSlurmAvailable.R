d59cfe47273396f000b4003cbec786efc8f0d0c6
Falk Benke <falk.benke@pik-potsdam.de>
2025-11-13 12:20:49 +0100

    disable slurm for workshop use

--- scripts/start/isSlurmAvailable.R
+++ scripts/start/isSlurmAvailable.R
@@ -10,5 +10,5 @@
 #'
 #' @return \code{logical(1)}. Whether slurm is available.
 isSlurmAvailable <- function() {
-  return(suppressWarnings(system2("srun", stdout = FALSE, stderr = FALSE) != 127))
+  return(FALSE)
 }
